3a14840df1a813855d3350030ed7aa6a35077e15
nicky-mc <cinimod@gmail.com>
2024-10-13 15:35:10 +0100

    refilling in paths for Main.jsx

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -15,4 +16,11 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 3000, // Adjust the chunk size warning limit to 3000 kB
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      Client: path.resolve(__dirname, "./Client"),
+    },
+  },
+  base: "./", // This ensures that asset URLs are relative
 });
